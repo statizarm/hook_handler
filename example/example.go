02b1eaf15634ffa06eab95fa0895106985361873
statizarm <statiz.arm@gmail.com>
2021-03-08 14:40:23 +0300

    Добавил шаблон проекта для golang - Описал в README.md основные принципы архитектуры репозитория - Добавил пример правильного импортирования пакетов - Добавил 2 фичи которые можно настроить в goland'е

---
+++ example/example.go
@@ -0,0 +1,10 @@
+package example
+
+import (
+	"fmt"
+)
+
+// Example is an example function
+func Example() {
+	fmt.Println("hello")
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/go_proj_example/example"
+)
+
+func main() {
+	example.Example()
+}
